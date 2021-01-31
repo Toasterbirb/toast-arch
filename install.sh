bea0419c9f4cd15c352d6d82e7b3c7c67e200315
Toasterbirb <git.53jvg@aleeas.com>
2021-01-31 17:49:12 +0200

    Install vim plugins automatically

--- install.sh
+++ install.sh
@@ -88,4 +88,4 @@ echo "exec dwm" >> ~/.xinitrc
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-nvim -c :PlugInstall
+nvim -c ":PlugInstall" -c ":q" -c ":q"
